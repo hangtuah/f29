81721fd2ec5a81a980eb3bd7bbf68b5b5d8d4819
MOHD FAIRUS AHMAD <mfairusahmad@utm.my>
2021-11-23 00:24:15 +0800

    init commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Tempahan Kenderaan</title>
+</head>
+<body>
+    <ul>
+    <li><a href="index.php">Utama</a></li>
+        <li> <a href="pengguna.php">Pengguna</a></li>
+        <li>Kenderaan</li>
+        <li>Tempahan</li>
+    </ul>
+</body>
+</html>
